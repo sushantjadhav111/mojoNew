a44ad5fe0de565c462dcdddc3ed6621dd03b185e
Sushant Jadhav <71134943+sushantjadhav111@users.noreply.github.com>
2026-03-24 18:04:44 +0530

    Add Footer component for the landing page

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p>&copy; 2023 Mojo Travel. All Rights Reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
