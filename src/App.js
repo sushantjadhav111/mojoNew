b0dae6f9236d6f49576fb5ac1e1558607374ffa8
Sushant Jadhav <71134943+sushantjadhav111@users.noreply.github.com>
2026-03-24 17:29:00 +0530

    Add initial boilerplate code for landing page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Mojo Travel</h1>
+        <p>Your adventure starts here!</p>
+        <button>Book Now</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
